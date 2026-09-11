4f6778c4b1b31b6292a831399e95d715a080e062
Michael Gitonga <gitoshmbae@gmail.com>
2026-09-11 11:19:26 +0300

    Updates the drawer to be more responsive and have scroll capability on mobile view in OurWorkSection.tsx

--- components/portFolioPage/OurWorkSection.tsx
+++ components/portFolioPage/OurWorkSection.tsx
@@ -34,7 +34,7 @@ export default function OurWorkSection() {
                     "object-cover absolute rounded-sm top-0 h-full w-full z-10"
                   }
                 />
-                <div className="absolute bottom-0 bg-fuchsia-100/50 backdrop-blur-md  rounded-tr-sm  z-20">
+                <div className="absolute bottom-0 bg-fuchsia-100/50 backdrop-blur-md rounded-tr-sm  z-20">
                   <div className="relative px-2 py-1 text-slate-900">
                     <h3 className="text-sm font-semibold drop-shadow-md">
                       {work.name}
@@ -50,33 +50,35 @@ export default function OurWorkSection() {
                           </Button>
                         }
                       />
-                      <DrawerContent className="min-h-[75dvh]  lg:min-w-7xl mx-auto">
+                      <DrawerContent className="min-h-[75dvh]  md:min-w-2xl lg:min-w-7xl mx-auto backdrop-blur-md rounded-tr-none rounded-br-none">
                         <DrawerHeader>
                           <DrawerTitle></DrawerTitle>
                           <DrawerDescription></DrawerDescription>
                         </DrawerHeader>
-                        <div className="relative min-h-[50dvh]  max-w-sm md:max-w-6xl mx-auto">
-                          <CloudinaryImage
-                            imgSrc={work.imgSrc}
-                            height={400}
-                            width={400}
-                            alt={`${work.name} image`}
-                            classNames={
-                              "object-cover h-[50dvh] w-[70dvw] rounded-sm"
-                            }
-                          />
-                        </div>
-                        <div className="max-w-7xl lg:ml-10 px-4 py-12 ">
-                          <h4 className="text-2xl md:text-4xl font-semibold pb-4">
-                            {work.name}
-                          </h4>
-                          <ul className="leading-8 list-disc">
-                            <li className="ml-2">{work.textOne}</li>
-                            <li className="ml-2">{work.textTwo}</li>
-                            <li className="ml-2">{work.textThree}</li>
-                            <li className="ml-2">{work.textFour}</li>
-                            <li className="ml-2">{work.textFive}</li>
-                          </ul>
+                        <div className="overflow-y-auto">
+                          <div className="relative min-h-[50dvh]  max-w-sm md:max-w-6xl mx-auto">
+                            <CloudinaryImage
+                              imgSrc={work.imgSrc}
+                              height={400}
+                              width={400}
+                              alt={`${work.name} image`}
+                              classNames={
+                                "object-cover h-[50dvh] w-[85dvw] lg:max-w-[70dvw] rounded-sm"
+                              }
+                            />
+                          </div>
+                          <div className="max-w-7xl lg:ml-10 px-4 py-12 ">
+                            <h4 className="text-2xl md:text-4xl font-semibold pb-4">
+                              {work.name}
+                            </h4>
+                            <ul className="leading-8 list-disc">
+                              <li className="ml-2">{work.textOne}</li>
+                              <li className="ml-2">{work.textTwo}</li>
+                              <li className="ml-2">{work.textThree}</li>
+                              <li className="ml-2">{work.textFour}</li>
+                              <li className="ml-2">{work.textFive}</li>
+                            </ul>
+                          </div>
                         </div>
                         <DrawerFooter className="grid place-content-center">
                           <DrawerClose
